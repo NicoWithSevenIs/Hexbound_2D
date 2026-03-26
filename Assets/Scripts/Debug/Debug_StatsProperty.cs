2f1c213c5ce958088d6366c94a38017a0acd8a1c
Enrico Tolentino <80933336+NicoWithSevenIs@users.noreply.github.com>
2026-03-26 21:52:51 +0800

    serializable dictionary

---
+++ Assets/Scripts/Debug/Debug_StatsProperty.cs
@@ -0,0 +1,16 @@
+using Hexbound.Stats;
+using UnityEngine;
+
+public class Debug_StatsProperty : MonoBehaviour
+{
+    [SerializeField] private Character stats;
+    [SerializeField] private Stats stat;
+
+
+    private void Start()
+    {
+        stat = stats.stats;
+    }
+
+
+}
---
+++ Assets/Scripts/Gameplay/Character/Data/ActionMultipliers.cs
@@ -0,0 +1,22 @@
+using Hexbound.Stats;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class Multiplier
+{
+    public StatType stat;
+    public float value;
+}
+
+[Serializable]
+public class ActionMultipliers
+{
+    [Header("Action Multipliers")]
+    public List<Multiplier> grounded_basic;
+    public List<Multiplier> grounded_heavy;
+    public List<Multiplier> aerial_basic;
+    public List<Multiplier> aerial_heavy;
+    public List<Multiplier> plunge;
+}
